8a0576111dbbba88d46a3b793bc4501a2d31e26f
edmeister <koed.dev@gmail.com>
2025-04-24 00:15:18 -0700

    fix: bug

--- tests/test_landing_page.py
+++ tests/test_landing_page.py
@@ -2,6 +2,7 @@ import pytest
 from po.landing_page import LandingPage
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 
 
 #  poetry run pytest --html=reports/report.html tests/.
@@ -9,16 +10,18 @@ class TestLandingPage:
 
     def test_landing_page_google(self, driver):
         landing_page = LandingPage(driver)
-        landing_page.open_landing_page()
+        landing_page.open_url(
+            "https://your-landing-page.com"
+        )  # Or use `open_landing_page()` if defined
+
+        locator = (By.XPATH, "//a[contains(text(), 'Google')]")
 
         try:
-            google_header = landing_page.find(
-                (By.XPATH, "//a[contains(text(), 'Google')]")
-            )
-            assert google_header.is_displayed(), "Google element displayed."
-        except NoSuchElementException:
+            landing_page.wait_until_element_is_visible(locator)
+            assert landing_page.is_displayed(locator), "Google link should be visible"
+        except TimeoutException or NoSuchElementException:
             landing_page.save_screenshot(self.test_landing_page_google.__name__)
-            raise AssertionError("Google header does not exist.")
+            raise AssertionError("Google link not visible in time")
 
     # def test_landing_page_amazon(self, driver):  # will fail on purpose
     #     landing_page = LandingPage(driver)
