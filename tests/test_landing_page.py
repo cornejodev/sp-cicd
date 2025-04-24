39b1753ec58ef5c14e99fe98c83a663afb7c5454
edmeister <koed.dev@gmail.com>
2025-04-24 00:39:15 -0700

    update: test cases

--- tests/test_landing_page.py
+++ tests/test_landing_page.py
@@ -7,17 +7,35 @@ from selenium.common.exceptions import TimeoutException
 
 
 #  poetry run pytest --html=reports/report.html tests/.
+# HEADLESS=false poetry run pytest --html=reports/report.html tests/
+# BROWSER=firefox HEADLESS=false poetry run pytest --html=reports/report.html tests/
 
 
 class TestLandingPage:
 
-    def test_landing_page_google(self, driver):
+    def test_landing_page_google_pass(self, driver):
         landing_page = LandingPage(driver)
-        landing_page.open_landing_page()
+        landing_page.open_url("https://www.google.com/")
 
-        assert (
-            landing_page.current_url == "https://www.google.com/"
-        ), f"Expected URL to be 'https://www.google.com/', but got '{landing_page.current_url}'"
+        try:
+            assert (
+                landing_page.current_url == "https://www.google.com/"
+            ), f"Expected URL to be 'https://www.google.com/', but got '{landing_page.current_url}'"
+        except AssertionError as e:
+            landing_page.save_screenshot(self.test_landing_page_google.__name__)
+            raise e
+
+    def test_landing_page_google_fail(self, driver):  # this will fail on purpose
+        landing_page = LandingPage(driver)
+        landing_page.open_url("https://www.amazon.com/")
+
+        try:
+            assert (
+                landing_page.current_url == "https://www.google.com/"
+            ), f"Expected URL to be 'https://www.google.com/', but got '{landing_page.current_url}'"
+        except AssertionError as e:
+            landing_page.save_screenshot(self.test_landing_page_google.__name__)
+            raise e
 
     # def test_landing_page_amazon(self, driver):  # will fail on purpose
     #     landing_page = LandingPage(driver)
